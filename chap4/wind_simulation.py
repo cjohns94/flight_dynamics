c7bbefb8466a760e9572dd87bf4c11dc45d9d77f
Curtis Johnson <curtiscarljohnson@gmail.com>
2021-02-03 15:59:40 -0700

    added wind sim stuff

--- chap4/wind_simulation.py
+++ chap4/wind_simulation.py
@@ -4,45 +4,63 @@ calculates a steady wind speed and uses a stochastic
 process to represent wind gusts. (Follows section 4.4 in uav book)
 """
 import sys
-sys.path.append('..')
+
+sys.path.append("..")
 from tools.transfer_function import transferFunction
+import parameters.aerosonde_parameters as param
 import numpy as np
 
 
 class WindSimulation:
     def __init__(self, Ts):
         # steady state wind defined in the inertial frame
-        self._steady_state = np.array([[0., 0., 0.]]).T
-        #self._steady_state = np.array([[0., 5., 0.]]).T
+        self._steady_state = np.array([[0.0, 0.0, 0.0]]).T
+        # self._steady_state = np.array([[0., 5., 0.]]).T
 
         #   Dryden gust model parameters (section 4.4 UAV book)
-        Va =  # must set Va to a constant value
-        Lu = 
-        Lv = 
-        Lw = 
+        # values taken from table 4.1
+        Va = param.Va0  # must set Va to a constant value
+        Lu = 200
+        Lv = 200
+        Lw = 50
         gust_flag = True
-        if gust_flag==True:
-            sigma_u = 
-            sigma_v = 
-            sigma_w = 
+        if gust_flag is True:
+            sigma_u = 1.06
+            sigma_v = 1.06
+            sigma_w = 0.7
         else:
-            sigma_u = 
-            sigma_v = 
-            sigma_w = 
+            sigma_u = 0
+            sigma_v = 0
+            sigma_w = 0
 
         # Dryden transfer functions (section 4.4 UAV book)
-        self.u_w = #transferFunction()
-        self.v_w = #transferFunction()
-        self.w_w = #transferFunction()
+        self.u_w = transferFunction(
+            sigma_u * np.sqrt((2 * Va) / (np.pi * Lu)), np.array([1, Va / Lu])
+        )
+        self.v_w = transferFunction(
+            sigma_v
+            * np.sqrt((3 * Va) / (np.pi * Lv))
+            * np.array([1, Va / (np.sqrt(3) * Lv)]),
+            np.array([1, 2 * Va / Lv, (Va / Lv) ** 2]),
+        )
+        self.w_w = transferFunction(
+            sigma_w
+            * np.sqrt((3 * Va) / (np.pi * Lw))
+            * np.array([1, Va / (np.sqrt(3) * Lw)]),
+            np.array([1, 2 * Va / Lw, (Va / Lw) ** 2]),
+        )
         self._Ts = Ts
 
     def update(self):
         # returns a six vector.
         #   The first three elements are the steady state wind in the inertial frame
         #   The second three elements are the gust in the body frame
-        gust = np.array([[self.u_w.update(np.random.randn())],
-                         [self.v_w.update(np.random.randn())],
-                         [self.w_w.update(np.random.randn())]])
-        #gust = np.array([[0.],[0.],[0.]])
-        return np.concatenate(( self._steady_state, gust ))
-
+        gust = np.array(
+            [
+                [self.u_w.update(np.random.randn())],
+                [self.v_w.update(np.random.randn())],
+                [self.w_w.update(np.random.randn())],
+            ]
+        )
+        # gust = np.array([[0.],[0.],[0.]])
+        return np.concatenate((self._steady_state, gust))
